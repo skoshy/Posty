810ac58aa67575daf1fa5605c59c51e118656a7d
Stefan Koshy <noreply@skoshy.com>
2016-06-21 03:09:23 +0000

    Added some style to the page

--- app.js
+++ app.js
@@ -11,8 +11,13 @@ app.controller('MainCtrl', [
         $scope.addPost = function() {
             if (!$scope.title || $scope.title === '') return;
             
-            $scope.posts.push({title: $scope.title, upvotes: 0});
+            $scope.posts.push({title: $scope.title, upvotes: 0, link: $scope.link});
+            
             $scope.title = '';
+            $scope.link = '';
+        };
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes++;
         };
     }
 ]);
