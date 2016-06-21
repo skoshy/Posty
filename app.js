59d784da38b93b4fd6b7c6a947ea22bdb13ed737
Stefan Koshy <noreply@skoshy.com>
2016-06-21 20:29:55 +0000

    Added a comments feature to posts. Posts come with a default comment for now

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
 var app = angular.module('posty', ['ui.router']);
 
+/* Configuration */
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('home', {
             url: '/home',
             templateUrl: '/home.html',
             controller: 'MainCtrl',
+        })
+        .state('posts', {
+            url: '/posts/{id}',
+            templateUrl: '/posts.html',
+            controller: 'PostsCtrl',
         });
     
     $urlRouterProvider.otherwise('home');
 }]);
 
-
+/* Controllers */
 app.controller('MainCtrl', [
     '$scope', 'posts',
     function ($scope, posts) {
         $scope.test = 'Hello World!';
         $scope.posts = posts.posts;
         $scope.addPost = function() {
+            // Don't add the post if there's no title
             if (!$scope.title || $scope.title === '') return;
             
-            $scope.posts.push({title: $scope.title, upvotes: 0, link: $scope.link});
+            // Store the post
+            $scope.posts.push({
+                title: $scope.title,
+                upvotes: 0,
+                link: $scope.link,
+                comments: [
+                    {
+                        author: 'Joe',
+                        body: 'Cool!',
+                        upvotes: 0,
+                    }
+                ],
+            });
             
+            // Reset the form
             $scope.title = '';
             $scope.link = '';
         };
@@ -31,6 +51,14 @@ app.controller('MainCtrl', [
     }
 ]);
 
+app.controller('PostsCtrl', [
+    '$scope', '$stateParams', 'posts',
+    function ($scope, $stateParams, posts) {
+        $scope.post = posts.posts[$stateParams.id];
+    }
+]);
+
+/* Factories */
 app.factory('posts', [function() {
     var o = {
         posts: []
