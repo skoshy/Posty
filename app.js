a300e95e58853b45b46ac29a11225d6060fdb89f
Stefan Koshy <noreply@skoshy.com>
2016-06-21 02:34:45 +0000

    Now you can add your own posts with your own title

--- app.js
+++ app.js
@@ -4,4 +4,15 @@ app.controller('MainCtrl', [
     '$scope',
     function ($scope) {
         $scope.test = 'Hello World!';
-}]);
+        $scope.posts = [
+            {title: 'post 1', upvotes: 5},
+            {title: 'post 2', upvotes: 2},
+        ];
+        $scope.addPost = function() {
+            if (!$scope.title || $scope.title === '') return;
+            
+            $scope.posts.push({title: $scope.title, upvotes: 0});
+            $scope.title = '';
+        };
+    }
+]);
